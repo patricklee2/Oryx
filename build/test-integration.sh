e874c9986e0fdf0fb38a91a5fda64e5632b996f8
Dor Fire <dorfire@microsoft.com>
2019-03-22 13:52:04 -0700

    Removed explicit docker pulls

--- build/test-integration.sh
+++ build/test-integration.sh
@@ -16,14 +16,9 @@ fi
 source $REPO_DIR/build/__variables.sh
 
 echo
-echo Pulling docker images required for running tests against databases ...
-docker pull mysql/mysql-server:5.7
-docker pull postgres:9.5
-docker pull microsoft/mssql-server-linux:2017-CU12
-
-echo
 echo "Building and running tests..."
 testProjectName="Oryx.Integration.Tests"
 cd "$TESTS_SRC_DIR/$testProjectName"
-# Run non-AKS tests temporarily
+
+# Run only non-AKS tests temporarily
 dotnet test --filter E2E!=AKS --test-adapter-path:. --logger:"xunit;LogFilePath=$ARTIFACTS_DIR/testResults/$testProjectName.xml" -c $BUILD_CONFIGURATION
